9ec84fd291518f3270a30fbcb0659a8f9c850bf3
kleitich <kevleitich@gmail.com>
2022-06-19 22:37:37 +0300

    adding views and url for registering a user

--- app/views.py
+++ app/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'auth/register.html', context)
