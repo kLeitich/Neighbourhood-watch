ee539e77776535c05af1f3c49f3224116eeb7148
kleitich <kevleitich@gmail.com>
2022-06-20 06:42:58 +0300

    adding styling to index and abase html

--- watch/settings.py
+++ watch/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+   
 ]
 
 ROOT_URLCONF = 'watch.urls'
@@ -65,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
